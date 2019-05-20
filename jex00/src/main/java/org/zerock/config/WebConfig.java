383c93b6052bdccb85e09bde0ffb999b2e5072fc
bifos <jon89071@gmail.com>
2019-05-20 11:13:54 +0900

    코드로 배우는 스프링 웹 프로젝트 시작

---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,26 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+// web.xml을 대신하는 클래스
+public class WebConfig extends 
+	AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
