50780b040643566bcefe9f7bb1bf234e16e3d4fd
bifos <jon89071@gmail.com>
2019-05-28 15:53:37 +0900

    Part3 - 기본적인 웹 게시물 관리 // Chapter 11 화면처리 완료

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -28,6 +28,11 @@ public class BoardController {
 		model.addAttribute("list", service.getList());
 	}
 	
+	@GetMapping("/register")
+	public void register() {
+		
+	}
+	
 	@PostMapping("/register")
 	public String register(BoardVO board, RedirectAttributes rttr) {
 		
@@ -37,10 +42,10 @@ public class BoardController {
 		return "redirect:/board/list";
 	}
 	
-	@GetMapping("/get")
+	@GetMapping({"/get", "/modify"})
 	public void get(@RequestParam("bno") Long bno, Model model) {
 		
-		log.info("/get");
+		log.info("/get or modify");
 		model.addAttribute("board", service.get(bno));
 	}
 	
@@ -64,4 +69,5 @@ public class BoardController {
 		}
 		return "redirect:/board/list";
 	}
+	
 }
