db3c40803373c91a4f5e301464e908f804b5030e
bifos <jon89071@gmail.com>
2019-06-11 11:07:03 +0900

    Chapter 22 - 파일 업로드 상세 처리

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,14 +1,28 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.domain.AttachFileDTO;
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
 
 @Controller
 @Log4j
@@ -23,7 +37,7 @@ public class UploadController {
 	@PostMapping("/uploadFormAction")
 	public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
 		
-		String uploadFolder = "C:\\Users\\KBT\\Desktop\\eclipse_2019-03_workspace\\upload\\temp";
+		String uploadFolder = "C:\\Users\\KBT\\Desktop\\eclipse_2019-03_workspace\\upload";
 		
 		for (MultipartFile multipartFile : uploadFile) {
 			
@@ -47,12 +61,24 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	@PostMapping(value = "/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
 		
 		log.info("update ajax post..........");
 		
-		String uploadFolder = "C:\\Users\\KBT\\Desktop\\eclipse_2019-03_workspace\\upload\\temp";
+		List<AttachFileDTO> list = new ArrayList<>();
+		String uploadFolder = "C:\\Users\\KBT\\Desktop\\eclipse_2019-03_workspace\\upload";
+		
+		String uploadFolderPath = getFolder();
+		// make folder ------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+		log.info("upload path: " + uploadPath);
+		
+		if (uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		// make yyyy/MM/dd folder
 		
 		for(MultipartFile multipartFile : uploadFile) {
 			
@@ -60,19 +86,67 @@ public class UploadController {
 			log.info("Upload File Name: " + multipartFile.getOriginalFilename());
 			log.info("Upload File Size: " + multipartFile.getSize());
 			
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			String uploadFileName = multipartFile.getOriginalFilename();
 			
 			// IE has file path
 			uploadFileName = uploadFileName.substring(uploadFileName.lastIndexOf("\\") + 1);
 			log.info("only file name: " + uploadFileName);
+			attachDTO.setFileName(uploadFileName);
 			
-			File saveFile = new File(uploadFolder, uploadFileName);
+			// 파일명에 UUID 추가
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				// check image type file
+				if (checkImageType(saveFile)) {
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
+					
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
+					thumbnail.close();
+				}
+				
+				// add to List
+				list.add(attachDTO);
+				
 			} catch (Exception e) {
 				log.error(e.getMessage());
 			} // end catch
+			
 		} // end for
+		
+		return new ResponseEntity<>(list, HttpStatus.OK);
+	}
+	
+	private String getFolder() {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		
+		Date date = new Date();
+		
+		String str = sdf.format(date);
+		
+		return str.replace("-", File.separator);
+	}
+	
+	private boolean checkImageType(File file) {
+		
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			log.info(contentType);
+			return contentType.startsWith("image");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 }
